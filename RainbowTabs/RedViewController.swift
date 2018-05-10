c6b88f733efed9ad3f5c889e3ec615668b37cdb7
JohnGoure <johnsgithub@gmail.com>
2018-05-10 14:53:42 -0500

    A toy tab application.

---
+++ RainbowTabs/RedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by john goure on 5/10/18.
+//  Copyright © 2018 john goure. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tabBarItem.badgeValue = "!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
